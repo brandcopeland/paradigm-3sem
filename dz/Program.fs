7847395561c2913f3a999984459928d9e975718f
Volodya <gtapsplus1@yandex.kz>
2023-12-06 00:02:53 +0300

    lab3 added

---
+++ dz/Program.fs
@@ -0,0 +1,9 @@
+﻿// Использование List.filter и оператора |> для создания нового списка evenSquaredNumbers
+let numbers = [1; 2; 3; 4; 5; 6; 7; 8; 9; 10]
+
+let evenSquaredNumbers =
+    numbers
+    |> List.filter (fun x -> x % 2 = 0)
+    |> List.map (fun x -> x * x)
+
+printfn "%A" evenSquaredNumbers
